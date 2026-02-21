bb37feb738f91146a1042e6c519cb39808cc7645
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-02-21 19:23:00 +0000

    add harmony deployment

---
+++ contracts/SourceIndex/deployments/1666600000/config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+declare \
+  -A \
+  solc_version \
+  evm_version \
+  contract_address
+
+solc_version=(
+  ["1.0"]="0.8.24"
+)
+evm_version=(
+  ["1.0"]="paris"
+)
+contract_address=(
+  ["1.0"]="0xDFb999bBe26cCbaBafC6e3FaF93236Fa27CD7989"
+)
